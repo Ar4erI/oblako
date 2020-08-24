f5e48e41db89fc5873e1eeff9edf25a6e35f8838
Ar4er <vlad.skachenko@gmail.com>
2020-08-24 15:53:28 +0200

    fix pip

--- app/__init__.py
+++ app/__init__.py
@@ -11,15 +11,20 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.secret_key = 'f48481f5948f6ced640f5292e92dee0a0db71346'
 app.permanent_session_lifetime
+
 # Init LoginManager
 login_manager = LoginManager(app)
+
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 # Init db
 db = SQLAlchemy(app)
+
 # Init ma
 ma = Marshmallow(app)
+
 # Blueprints
 from users import users
 from admin import admin
